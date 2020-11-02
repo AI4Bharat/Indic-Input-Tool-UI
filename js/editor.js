e07512c4662951b2438af93e8de63084102add73
Gokul NC <gokulnc@ymail.com>
2020-11-02 16:35:30 +0530

    Refactor editor.js to new module exporter.js

--- js/editor.js
+++ js/editor.js
@@ -1,16 +1,7 @@
 var QUILL_EDITOR;
-var LAST_SAVE_TIMESTAMP = 0;
-var SAVE_CONTENT_FREQUENCY = 5*1000; // millisecs
-
-function saveContent() {
-    if (Date.now() - LAST_SAVE_TIMESTAMP > SAVE_CONTENT_FREQUENCY) {
-        localStorage.setItem('quill_content', QUILL_EDITOR.root.innerHTML);
-        LAST_SAVE_TIMESTAMP = Date.now();
-    }
-}
 
 async function handleChangesInText(searchTerm, renderList, mentionChar) {
-    saveContent();
+    saveQuillContentLocally(QUILL_EDITOR);
 
     if (!searchTerm) return;
     const lang = localStorage.getItem('lang');
@@ -61,64 +52,6 @@ function setLanguagesDropDown(dropDownItems, quill) {
         localStorage.setItem('lang_label', label);
     }
     myDropDown.attach(quill, true);
-}
-
-function downloadString(text, fileType, fileName) {
-    // https://gist.github.com/danallison/3ec9d5314788b337b682
-    var blob = new Blob([text], { type: fileType });
-  
-    var a = document.createElement('a');
-    a.download = fileName;
-    a.href = URL.createObjectURL(blob);
-    a.dataset.downloadurl = [fileType, a.download, a.href].join(':');
-    a.style.display = "none";
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-    setTimeout(function() { URL.revokeObjectURL(a.href); }, 1500);
-}
-
-function downloadQuillContent(format, quill) {
-    if (format == "pdf") {
-        var opt = {
-            margin:       1,
-            filename:     'transcript.pdf',
-            html2canvas:  { scale: 2 },
-            jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-        };
-        html2pdf().set(opt).from(quill.root.innerHTML).save();
-    }
-    else if (format == "txt") {
-        downloadString(quill.getText().trim(), "text/txt", "transcript.txt")
-    }
-    else if (format == "md") {
-        var turndownService = new TurndownService();
-        var markdown = turndownService.turndown(quill.root.innerHTML);
-        downloadString(markdown, "text/md", "transcript.md")
-    }
-    else if (format == "html") {
-        downloadString(quill.root.innerHTML, "text/html", "transcript.html")
-    }
-}
-
-function setupQuillExport(quill) {
-    const exportDropDown = new QuillToolbarDropDown({
-        label: "Export",
-        rememberSelection: false
-    });
-    
-    exportDropDown.setItems({
-        "PDF": "pdf",
-        "Text": "txt",
-        "Markdown": "md",
-        "HTML": "html"
-    });
-
-    exportDropDown.onSelect = function(label, value, quill) {
-        downloadQuillContent(value, quill);
-    }
-
-    exportDropDown.attach(quill);
 }
 
 var toolbarOptions = {
@@ -185,12 +118,7 @@ function setupQuillEditor() {
     });
 
     setupQuillExport(QUILL_EDITOR);
-
-    // Restore text content if present
-    const cached_content = localStorage.getItem('quill_content');
-    if (cached_content) {
-        QUILL_EDITOR.root.innerHTML = cached_content;
-    }
+    restoreQuillContentFromLocal(QUILL_EDITOR);
 }
 
 window.onload = () => {
