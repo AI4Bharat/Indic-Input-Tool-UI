63d6387eb997517e17a75a5cf9d6ce7146fef630
srihari07-web <sriharikrishna07@gmail.com>
2023-11-20 13:01:19 +0530

    api

--- js/api.js
+++ js/api.js
@@ -1,18 +1,17 @@
-const API_URL = "//xlit-api.ai4bharat.org";
+const API_URL =
+  "https://api.dhruva.ai4bharat.org/services/inference/transliteration";
 const LANGS_API = API_URL + "/languages";
 const LEARN_API = API_URL + "/learn";
-const TRANSLITERATE_API = API_URL + "/transliterate";
 
 async function getTransliterationSuggestions(lang, searchTerm) {
-
-  if (searchTerm == '.' || searchTerm == '..') {
-    searchTerm = ' ' + searchTerm;
+  if (searchTerm == "." || searchTerm == "..") {
+    searchTerm = " " + searchTerm;
   }
   searchTerm = encodeURIComponent(searchTerm);
 
   const url = `${API_URL}/tl/${lang}/${searchTerm}`;
   let response = await fetch(url, {
-    credentials: 'include'
+    credentials: "include",
   });
   let data = await response.json();
   return data;
@@ -20,59 +19,65 @@ async function getTransliterationSuggestions(lang, searchTerm) {
 
 async function getTransliterationForWholeText(inputLang, outputLang, text) {
   const data = {
-    "input": [
+    input: [
       {
-        "source": text
-      }
+        source: text,
+      },
     ],
-    "config": {
-      "isSentence": true,
-      "language": {
-        "sourceLanguage": inputLang,
-        "targetLanguage": outputLang
-      }
-    }
+    config: {
+      serviceId: "ai4bharat/indicxlit--cpu-fsv2",
+      language: {
+        sourceLanguage: "en",
+        sourceScriptCode: "",
+        targetLanguage: "hi",
+        targetScriptCode: "",
+      },
+      isSentence: true,
+      numSuggestions: 5,
+    },
+    controlConfig: {
+      dataTracking: true,
+    },
   };
 
-  const outputData = await fetch(TRANSLITERATE_API, {
-    method: 'post',
+  const outputData = await fetch(API_URL, {
+    method: "post",
     body: JSON.stringify(data),
-    mode: 'cors',
     headers: new Headers({
-      'Content-Type': 'application/json'
-    })
-  })
-  .then(response => response.json());
+      "Content-Type": "application/json",
+    }),
+  }).then((response) => response.json());
   return outputData["output"][0]["target"];
 }
 
 async function getSupportedLanguages() {
   let response = await fetch(LANGS_API, {
-    credentials: 'include' // To allow CORS cookies
+    credentials: "include",
   });
   let data = await response.json();
   return data;
 }
 
 async function recordUserSelection(lang, input, output, id) {
-  if (id < 0)
-    return;
+  if (id < 0) return;
   const data = {
-    "lang": lang,
-    "input": input,
-    "output": output,
-    "topk_index": id
-  }
-  xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("POST", LEARN_API, true);
-  xmlhttp.withCredentials = true;
-  xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4) {
-      if (xmlhttp.status != 200) {
-        console.log("ERROR: Failed to recordUserSelection(). Status: " + xmlhttp.status);
-      }
-    }
+    lang: lang,
+    input: input,
+    output: output,
+    topk_index: id,
   };
-  xmlhttp.setRequestHeader("Content-type", "application/json");
-  xmlhttp.send(JSON.stringify(data));
+  fetch(LEARN_API, {
+    method: "post",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json",
+    }),
+    credentials: "include",
+  }).then((response) => {
+    if (response.status !== 200) {
+      console.log(
+        "ERROR: Failed to recordUserSelection(). Status: " + response.status
+      );
+    }
+  });
 }
